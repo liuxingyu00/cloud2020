8e988d1841375844d0d5889bf0bf2cc60c79bfef
wz <2924651831@qq.com>
2022-11-17 14:21:50 +0800

    1

---
+++ cloud-api-commons/src/test/java/cn/wz/springcloud/Test.java
@@ -0,0 +1,12 @@
+package cn.wz.springcloud;
+
+/**
+ * @Author wz
+ * @Date 2022/11/17 14:15
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这行代码将推送到远程仓库");
+    }
+}
