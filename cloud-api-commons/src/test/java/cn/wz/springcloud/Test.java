fd29f8e9a8fe822af990515260d540c3903f0349
liuxingyu00 <63771604+liuxingyu00@users.noreply.github.com>
2022-11-17 14:36:53 +0800

    1

--- cloud-api-commons/src/test/java/cn/wz/springcloud/Test.java
+++ cloud-api-commons/src/test/java/cn/wz/springcloud/Test.java
@@ -8,5 +8,6 @@ package cn.wz.springcloud;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这行代码将推送到远程仓库");
+        System.out.println("这行代码将被拉到本地仓库");
     }
 }
